ee2e36bcdd66db2e1cf4034b4c80c622e65a8fcb
Bogdan Teslarasu <bogdan.teslarasu@doc-process.com>
2017-05-04 17:51:26 +0300

    Overriding JAVA_HOME when development environment is not set by default for JDK 9.

--- 2_Modules/run.sh
+++ 2_Modules/run.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+JDK_9_HOME=/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home
 javac --module-source-path src -d out $(find . -name '*.java')
 java --module-path out -m main/bookapp.BookApp;
